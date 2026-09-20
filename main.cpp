5dace2fc62610be0b24c759d7317445e09652403
Vera Cherkashina <veracherkashina17@gmail.com>
2026-09-20 16:49:23 +0300

    Add function Cherkashina

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void functionCherkashina();
+
+int main() {
+    std::cout << "Main program started.\n";
+    
+    functionCherkashina();
+    
+    return 0;
+}
+
+void functionCherkashina() {
+    std::cout << "[Cherkashina]: Hello! This is my contribution to the project.\n";
+}
