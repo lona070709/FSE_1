afca050a9b77f8628837d5ca65a0121b6d242a82
verastth <veracherkashina17@gmail.com>
2026-09-21 13:42:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,2 @@
-#include <iostream>
-
-void functionCherkashina();
-
-int main() {
-    std::cout << "Main program started.\n";
-    
-    functionCherkashina();
-    
-    return 0;
-}
-
 void functionCherkashina() {
-    std::cout << "[Cherkashina]: Hello! This is my contribution to the project.\n";
-}
+    std::cout << "[Cherkashina]: Hello, from my function!\n"
